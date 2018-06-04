7dfb901b71c1b86b014f6d8c925ef8ba1d763e90
Alessandro Romanelli <aleromrod@gmail.com>
2018-06-04 16:50:36 +0200

    Changed loader interactivity and background responsiveness

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,28 @@
-$(document).ready(function() {
+function toggleLoader() {
   if (sessionStorage.hasVisited) {
     $('.cover').css('display', 'none')
   } else {
     sessionStorage.hasVisited = true
     $('body').css('overflow', 'hidden')
   }
+  $('.background').hover(function () {
+    $('#logo img').removeClass('heartbeat');
+  }, function () {
+    $('#logo img').addClass('heartbeat');
+  })
   $('.background').on('click', function() {
-    $(this).parent().addClass("hidden")
+    $('.cover').addClass("hidden")
     $('body').css('overflow', 'auto')
   })
+  setTimeout(function() {
+    $('.cover').addClass("hidden");
+    $('body').css('overflow', 'auto')
+    setTimeout(function () {
+      $('.cover').remove();
+    }, 500)
+  }, 5000);
+}
+
+$(document).ready(function() {
+  toggleLoader();
 })
